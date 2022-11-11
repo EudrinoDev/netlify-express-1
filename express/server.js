ba8453c224159c157782712ffcd7d00ea3af7b44
EudrinoDev <57170855+EudrinoDev@users.noreply.github.com>
2022-11-11 14:01:51 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -7,8 +7,9 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
+  let p = JSON.stringify(process);
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write(p);
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
